b98c249f70fff0a3cac805e7fe38df03ae7078f6
Bastian Drüsedow <62537139+bdruesedow-imp@users.noreply.github.com>
2020-03-31 10:01:16 +0200

    add action

---
+++ versioning.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+  const ref = core.getInput('github_ref');
+
+  const buildNr = core.getInput('github_run_number');
+  console.log("Ref:", ref);
+
+  const branchName = ref.split("/").slice(-1).toString();
+  console.log("Branch:", branch);
+
+  var apppendix;
+
+  switch(true) {
+    // master branch only
+    case (ref.endsWith("master")):
+      appendix = "RELEASE." + buildNr;
+      break;
+    // only when release branch without additional branch name
+    case (ref.endsWith("release")):
+      appendix = "RC." + buildNr;
+      break;
+    // release with additional branch name e.g. "/release/2020-03"
+    case (ref.includes("/release/")):
+      appendix = "RC-" + branchName + "." + buildNr;
+      break;
+    case (ref.endsWith("develop")):
+      appendix = "SNAPSHOT." + buildNr;
+      break;
+    default:
+      appendix = "SNAPSHOT-" + branchName + "." + buildNr;
+      break;
+  }
+
+  console.log(appendix);
+  core.setOutput("appendix", appendix);
+
+} catch (error) {
+  core.setFailed(error.message);
+
+}
