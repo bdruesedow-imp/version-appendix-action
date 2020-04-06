7bb8a59500c08dd7c1cb37f55ef9500ab37ad422
Bastian Drüsedow <62537139+bdruesedow-imp@users.noreply.github.com>
2020-04-06 08:39:37 +0200

    remove github run number in script
    - run number is still an optional value, so workflows will not crash at the moment
    
    BREAKING CHANGE: remove github run number

--- versioning.js
+++ versioning.js
@@ -4,8 +4,6 @@ const github = require('@actions/github');
 try {
 
   const ref = core.getInput('github-ref');
-
-  const buildNr = core.getInput('github-run-number');
   console.log("Ref:", ref);
 
   const branchName = ref.split("/").slice(-1).toString();
@@ -16,21 +14,21 @@ try {
   switch(true) {
     // master branch only
     case (ref.endsWith("master")):
-      appendix = "RELEASE." + buildNr;
+      appendix = "RELEASE";
       break;
     // only when release branch without additional branch name
     case (ref.endsWith("release")):
-      appendix = "RC." + buildNr;
+      appendix = "RC";
       break;
     // release with additional branch name e.g. "/release/2020-03"
     case (ref.includes("/release/")):
-      appendix = "RC-" + branchName + "." + buildNr;
+      appendix = "RC-" + branchName;
       break;
     case (ref.endsWith("develop")):
       appendix = "SNAPSHOT." + buildNr;
       break;
     default:
-      appendix = "SNAPSHOT-" + branchName + "." + buildNr;
+      appendix = "SNAPSHOT-" + branchName;
       break;
   }
 
